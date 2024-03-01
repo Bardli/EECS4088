f4b813dfaf3ba1f028fca32a26b7b436ad476544
Bardli <baidu_li_2001@163.com>
2024-03-01 22:39:30 +0000

    fir

--- perfTestLED/gen/main.pp
+++ perfTestLED/gen/main.pp
@@ -1,5 +1,6 @@
 gen/main.object: main.c
 gen/main.object: /usr/share/ti/cgt-pru/include/stdint.h
+gen/main.object: /usr/share/ti/cgt-pru/include/_stdint40.h
 gen/main.object: /usr/lib/ti/pru-software-support-package/include/am335x/pru_cfg.h
 gen/main.object: resource_table_empty.h
 gen/main.object: /usr/share/ti/cgt-pru/include/stddef.h
